0097487c7680d1f3f71f5433b54d10f2dc5ca5b8
Brad Fol <bradfol@gmail.com>
2015-12-02 17:32:04 -0800

    Replace with files from AnyPhone project

--- cloud/main.js
+++ cloud/main.js
@@ -1,34 +1,101 @@
+require("cloud/app.js");
 
+var twilioAccountSid = 'Your-Twilio-Account-Sid';
+var twilioAuthToken = 'Your-Twilio-Auth-Token';
+var twilioPhoneNumber = 'Your-Twilio-Phone-Number';
+var secretPasswordToken = 'Something-Random-Here';
 
-var Parse = require('parse-cloud-express').Parse;
+var language = "en";
+var languages = ["en", "es", "ja", "kr", "pt-BR"];
 
+var twilio = require('twilio')(twilioAccountSid, twilioAuthToken);
 
-Parse.Cloud.define("hello", function(request, response) {
-  console.log('Ran cloud function.');
-  // As with Parse-hosted Cloud Code, the user is available at: request.user
-  // You can get the users session token with: request.user.getSessionToken()
-  // Use the session token to run other Parse Query methods as that user, because
-  //   the concept of a 'current' user does not fit in a Node environment.
-  //   i.e.  query.find({ sessionToken: request.user.getSessionToken() })...
-  response.success("Hello world! " + (request.params.a + request.params.b));
+Parse.Cloud.define("sendCode", function(req, res) {
+	var phoneNumber = req.params.phoneNumber;
+	phoneNumber = phoneNumber.replace(/\D/g, '');
+
+	var lang = req.params.language;
+  if(lang !== undefined && languages.indexOf(lang) != -1) {
+		language = lang;
+	}
+
+	if (!phoneNumber || (phoneNumber.length != 10 && phoneNumber.length != 11)) return res.error('Invalid Parameters');
+	Parse.Cloud.useMasterKey();
+	var query = new Parse.Query(Parse.User);
+	query.equalTo('username', phoneNumber + "");
+	query.first().then(function(result) {
+		var min = 1000; var max = 9999;
+		var num = Math.floor(Math.random() * (max - min + 1)) + min;
+
+		if (result) {
+			result.setPassword(secretPasswordToken + num);
+			result.set("language", language);
+			result.save().then(function() {
+				return sendCodeSms(phoneNumber, num, language);
+			}).then(function() {
+				res.success();
+			}, function(err) {
+				res.error(err);
+			});
+		} else {
+			var user = new Parse.User();
+			user.setUsername(phoneNumber);
+			user.setPassword(secretPasswordToken + num);
+			user.set("language", language);
+			user.setACL({});
+			user.save().then(function(a) {
+				return sendCodeSms(phoneNumber, num, language);
+			}).then(function() {
+				res.success();
+			}, function(err) {
+				res.error(err);
+			});
+		}
+	}, function (err) {
+		res.error(err);
+	});
 });
 
+Parse.Cloud.define("logIn", function(req, res) {
+	Parse.Cloud.useMasterKey();
 
-Parse.Cloud.beforeSave('TestObject', function(request, response) {
-  console.log('Ran beforeSave on objectId: ' + request.object.id);
-  response.success();
+	var phoneNumber = req.params.phoneNumber;
+	phoneNumber = phoneNumber.replace(/\D/g, '');
+
+	if (phoneNumber && req.params.codeEntry) {
+		Parse.User.logIn(phoneNumber, secretPasswordToken + req.params.codeEntry).then(function (user) {
+			res.success(user.getSessionToken());
+		}, function (err) {
+			res.error(err);
+		});
+	} else {
+		res.error('Invalid parameters.');
+	}
 });
 
-Parse.Cloud.afterSave('TestObject', function(request, response) {
-  console.log('Ran afterSave on objectId: ' + request.object.id);
-});
+function sendCodeSms(phoneNumber, code, language) {
+	var prefix = "+1";
+	if(typeof language !== undefined && language == "ja") {
+		prefix = "+81";
+	} else if (typeof language !== undefined && language == "kr") {
+		prefix = "+82";
+		phoneNumber = phoneNumber.substring(1);
+	} else if (typeof language !== undefined && language == "pt-BR") {
+		prefix = "+55";
+  }
 
-Parse.Cloud.beforeDelete('TestObject', function(request, response) {
-  console.log('Ran beforeDelete on objectId: ' + request.object.id);
-  response.success();
-});
-
-Parse.Cloud.afterDelete('TestObject', function(request, response) {
-  console.log('Ran afterDelete on objectId: ' + request.object.id);
-});
-
+	var promise = new Parse.Promise();
+	twilio.sendSms({
+		to: prefix + phoneNumber.replace(/\D/g, ''),
+		from: twilioPhoneNumber.replace(/\D/g, ''),
+		body: 'Your login code for AnyPhone is ' + code
+	}, function(err, responseData) {
+		if (err) {
+			console.log(err);
+			promise.reject(err.message);
+		} else {
+			promise.resolve();
+		}
+	});
+	return promise;
+}
